2bbddd4ecc1b6749c3bf04efd861a473c4398e9d
Pruthvi <pruthvimogaveer18@gmail.com>
2024-04-21 22:22:27 +0530

    Time: 85 ms (21.72%), Space: 53.7 MB (16.87%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var myAtoi = function (s) {
+    let i = 0;
+
+    while (s[i] === ' ') {
+        i++;
+    }
+
+    let sign = 1;
+    if (s[i] === '-' || s[i] === '+') {
+        sign = s[i] == '-' ? -1 : 1;
+        i++;
+    }
+    
+    let num = ""
+    while (s[i] >= '0' && s[i] <= '9') {
+        num = num + s[i];
+        i++;
+    }
+    num *= sign;
+    if (num < -(2 ** 31) || num > (2 ** 31) - 1) {
+        return num < -(2 ** 31) ? -(2 ** 31) : (2 ** 31) - 1
+    }
+
+    return num;
+};
