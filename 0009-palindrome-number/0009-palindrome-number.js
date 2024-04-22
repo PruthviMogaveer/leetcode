02053668830a73774f448de4628b55623fb2a03d
Pruthvi <pruthvimogaveer18@gmail.com>
2024-04-22 10:05:50 +0530

    Time: 144 ms (32.05%), Space: 56.2 MB (96.92%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.js
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -3,14 +3,14 @@
  * @return {boolean}
  */
 var isPalindrome = function(x) {
-    if(x<0)
-        return false
+    if(x<0) return false
 
     let rev = 0
     let temp = x
-    while(temp > 0){
-        rev = rev * 10 + temp % 10
-        temp = Math.floor(temp / 10)
+
+    while(x > 0){
+        rev = rev * 10 + x % 10
+        x = Math.floor(x / 10)
     }
-    return x == rev 
+    return temp == rev 
 }
