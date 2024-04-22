5d30ce2b0647096c5f4667877f56a18da3e84590
Pruthvi <pruthvimogaveer18@gmail.com>
2024-04-22 10:46:07 +0530

    Time: 428 ms (32.21%), Space: 50.2 MB (84.19%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.js
+++ 0010-regular-expression-matching/0010-regular-expression-matching.js
@@ -4,13 +4,12 @@
  * @return {boolean}
  */
 var isMatch = function(s, p) {
-    if (p === "") return s === "";
-     const firstMatch = s !== "" && (p[0] === s[0] || p[0] === '.');
 
-    if (p.length >= 2 && p[1] === '*') {
-        return (isMatch(s, p.substring(2)) || (firstMatch && isMatch(s.substring(1), p)));
-    } else {
-        return firstMatch && isMatch(s.substring(1), p.substring(1));
+    try {
+        return new RegExp("^" + p + "$").test(s);
+    } catch (error) {
+        // Invalid regular expression
+        console.log(s,p)
+        return isMatch(s,p.substring(1))
     }
-
 };
