56c6ac0a8df6f2ceb60ab2b68da2a610ad15cdb4
Pruthvi <pruthvimogaveer18@gmail.com>
2024-04-23 09:40:44 +0530

    Time: 75 ms (21.66%), Space: 56.5 MB (94.62%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.js
+++ 0011-container-with-most-water/0011-container-with-most-water.js
@@ -9,8 +9,9 @@ var maxArea = function (height) {
     if (height.length == 2) return Math.min(...height)
 
     while(start<end){
-        if (Math.min(height[start], height[end]) * (end - start) > result) 
-            result = Math.min(height[start], height[end]) * (end - start)
+        let tempRes = Math.min(height[start], height[end]) * (end - start)
+        if ( tempRes > result) 
+            result = tempRes
         if(height[start] <= height[end])
             start++
         else
