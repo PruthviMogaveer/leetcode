994870507bfc2408ea94a28ea33550ff102604dd
Pruthvi <pruthvimogaveer18@gmail.com>
2024-04-23 20:13:57 +0530

    Time: 56 ms (56.05%), Space: 49.4 MB (59.77%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+    if(strs.length == 1 || strs[0] == "") return strs[0]
+    strs = strs.sort()
+    let len = Math.max(strs[0].length, strs[strs.length-1].length)
+    let res = 0 
+
+    for(let i=0;i<len;i++){
+        if(strs[0][i] == strs[strs.length-1][i])
+            res+=1
+        else
+            return strs[0].slice(0,res)     
+    }
+    return strs[0].slice(0,res)
+};
