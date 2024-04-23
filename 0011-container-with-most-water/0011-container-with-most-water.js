09abc78fa212e64d4c3b29a1fe05f70114b66284
Pruthvi <pruthvimogaveer18@gmail.com>
2024-04-23 09:39:04 +0530

    Time: 67 ms (57.93%), Space: 57.1 MB (55.61%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function (height) {
+    let start = 0
+    let end = height.length - 1
+    let result = 0
+    if (height.length == 2) return Math.min(...height)
+
+    while(start<end){
+        if (Math.min(height[start], height[end]) * (end - start) > result) 
+            result = Math.min(height[start], height[end]) * (end - start)
+        if(height[start] <= height[end])
+            start++
+        else
+            end--
+    }
+    return result
+};
