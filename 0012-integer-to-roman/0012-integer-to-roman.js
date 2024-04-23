75e13ba2c02a1bf159d8b7e4a23718fdccee3198
Pruthvi <pruthvimogaveer18@gmail.com>
2024-04-23 10:18:30 +0530

    Time: 109 ms (72.89%), Space: 54.1 MB (84.27%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.js
+++ 0012-integer-to-roman/0012-integer-to-roman.js
@@ -3,28 +3,10 @@
  * @return {string}
  */
 var intToRoman = function(num) {
-    const romanMap = [
-        { value: 1000, symbol: 'M' },
-        { value: 900, symbol: 'CM' },
-        { value: 500, symbol: 'D' },
-        { value: 400, symbol: 'CD' },
-        { value: 100, symbol: 'C' },
-        { value: 90, symbol: 'XC' },
-        { value: 50, symbol: 'L' },
-        { value: 40, symbol: 'XL' },
-        { value: 10, symbol: 'X' },
-        { value: 9, symbol: 'IX' },
-        { value: 5, symbol: 'V' },
-        { value: 4, symbol: 'IV' },
-        { value: 1, symbol: 'I' }
-    ];
+    const M = ["", "M", "MM", "MMM",];
+    const C = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"];
+    const X = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"];
+    const I = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
 
-    let result = '';
-    for (const { value, symbol } of romanMap) {
-        while (num >= value) {
-            result += symbol;
-            num -= value;
-        }
-    }
-    return result;
+    return M[Math.floor(num / 1000)] + C[Math.floor((num % 1000) / 100)] + X[Math.floor((num % 100) / 10)] + I[num % 10];
 };
