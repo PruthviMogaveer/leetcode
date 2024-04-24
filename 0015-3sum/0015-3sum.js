7b9344b7859bdb5bd3759c3e2d5227893c7d8bbe
Pruthvi <pruthvimogaveer18@gmail.com>
2024-04-24 21:28:48 +0530

    Time: 157 ms (69.39%), Space: 65.5 MB (47.64%) - LeetHub

---
+++ 0015-3sum/0015-3sum.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function (nums) {
+    let res = [];
+    nums.sort((a, b) => a - b);
+
+    for (let start = 0; start < nums.length - 2; start++) {
+        if (start > 0 && nums[start] === nums[start - 1]) continue; // Skip duplicates
+
+        let mid = start + 1;
+        let end = nums.length - 1;
+
+        while (mid < end) {
+            let sum = nums[start] + nums[mid] + nums[end];
+            if (sum === 0) {
+                res.push([nums[start], nums[mid], nums[end]]);
+                mid++;
+                end--;
+
+                // Skip duplicates
+                while (mid < end && nums[mid] === nums[mid - 1]) mid++;
+                while (mid < end && nums[end] === nums[end + 1]) end--;
+            } else if (sum < 0) {
+                mid++;
+            } else {
+                end--;
+            }
+        }
+    }
+
+    return res;
+};
