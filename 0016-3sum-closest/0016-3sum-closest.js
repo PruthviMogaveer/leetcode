ee3b7a0ce9abf83025233f5e9639c3e693683c2c
Pruthvi <pruthvimogaveer18@gmail.com>
2024-04-24 22:11:23 +0530

    Time: 78 ms (27.2%), Space: 51.1 MB (37.82%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.js
+++ 0016-3sum-closest/0016-3sum-closest.js
@@ -3,9 +3,9 @@
  * @param {number} target
  * @return {number}
  */
-var threeSumClosest = function(nums, target) {
+var threeSumClosest = function (nums, target) {
     nums.sort((a, b) => a - b);
-    let closestSum = nums[0]+nums[1]+nums[2]
+    let closestSum = nums[0] + nums[1] + nums[2]
 
     for (let start = 0; start < nums.length - 2; start++) {
         let mid = start + 1;
@@ -14,17 +14,13 @@ var threeSumClosest = function(nums, target) {
         while (mid < end) {
             let sum = nums[start] + nums[mid] + nums[end];
 
-            if (Math.abs(sum - target) < Math.abs(closestSum - target)) {
+            if (Math.abs(sum - target) < Math.abs(closestSum - target))
                 closestSum = sum;
-            }
-
-            if (sum === target) {
-                return target;
-            } else if (sum < target) {
+            if (sum === target) return target;
+            else if (sum < target)
                 mid++;
-            } else {
+            else
                 end--;
-            }
         }
     }
 
