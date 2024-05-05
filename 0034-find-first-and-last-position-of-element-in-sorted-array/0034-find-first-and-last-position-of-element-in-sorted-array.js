728101c390aa247dbc62f366c74110563db95eb4
Pruthvi <pruthvimogaveer18@gmail.com>
2024-05-05 09:51:06 +0530

    Time: 58 ms (27.38%), Space: 49 MB (95.18%) - LeetHub

---
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var searchRange = function(nums, target) {
+    if(nums.includes(target)){
+        let start = nums.indexOf(target)
+        for(let i=nums.length-1;i>=0;i--){
+            if(nums[i]==target){
+                end = i
+                break;
+            }
+        }
+        return [start,end]
+    }
+    else
+        return [-1,-1]
+};
